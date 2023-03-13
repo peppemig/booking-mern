20aa81a6d0c26ec38fa35ed7f262419adec6a1a9
peppemig <120139042+peppemig@users.noreply.github.com>
2023-03-13 02:46:53 +0100

    started fetching items from backend

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
 import './featured.css'
 
 const Featured = () => {
+
+    const {data,loading,error} = useFetch("http://localhost:8800/api/hotels/countyByCity?cities=madrid,london,berlin")
+
   return (
     <div className="featured">
 
+        {loading ? 
+            ("Loading please wait") : (
+        <>
         <div className="featuredItem">
             <img src="https://cf.bstatic.com/xdata/images/city/600x600/613105.jpg?k=1e85cf4dec7b0d5a6327be91c38cf9c1711f9da1a31c4cba736f9cb751443ff1&o=" alt="" className="featuredImg" />
             <div className="featuredTitles">
-                <h1>Rome</h1>
-                <h2>436 properties</h2>
+                <h1>Madrid</h1>
+                <h2>{data[0]} properties</h2>
             </div>
         </div>
         <div className="featuredItem">
             <img src="https://cf.bstatic.com/xdata/images/city/600x600/641091.jpg?k=6b4c99444b439d2ce4dbf5a479674965a4f196f4cab39bf7dd34e214f1d30b83&o=" alt="" className="featuredImg" />
             <div className="featuredTitles">
-                <h1>Naples</h1>
-                <h2>241 properties</h2>
+                <h1>London</h1>
+                <h2>{data[1]} properties</h2>
             </div>
         </div>
         <div className="featuredItem">
             <img src="https://cf.bstatic.com/xdata/images/city/600x600/619644.jpg?k=5b21ac6d3d913a54274d6546e60adb2c3c138d661dd1a2bd7b3aed53e9df0b65&o=" alt="" className="featuredImg" />
             <div className="featuredTitles">
-                <h1>Milan</h1>
-                <h2>739 properties</h2>
+                <h1>Berlin</h1>
+                <h2>{data[2]} properties</h2>
             </div>
         </div>
+        </>)}
         
     </div>
   )
--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,45 +1,37 @@
 import React from 'react'
 import './propertyList.css'
+import useFetch from '../../hooks/useFetch'
 
 const PropertyList = () => {
+
+    const {data,loading,error} = useFetch("http://localhost:8800/api/hotels/countByType")
+
+    //console.log(data[0].type)
+    const images = [
+        "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=",
+        "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/119467716.jpeg?k=63b69100225782d08fbd4d0205bf949c0be894ab946a0366edb8ad48e9c0ef46&o=",
+        "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450084.jpeg?k=f8c2954e867a1dd4b479909c49528531dcfb676d8fbc0d60f51d7b51bb32d1d9&o=",
+        "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/100235855.jpeg?k=61ef6692e05b5971e2e8dc75687f844e6d0ad295a9a5ace17f7c713f167e61b5&o=",
+        "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450074.jpeg?k=7039b03a94f3b99262c4b3054b0edcbbb91e9dade85b6efc880d45288a06c126&o="
+    ]
+
   return (
     <div className="pList">
 
-        <div className="pListItem">
-            <img className="pListImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" />
+        {loading ? ("Loading") : (
+        <>
+
+        {data && data.map((item,i) => (
+        <div className="pListItem" key={i}>
+            <img className="pListImg" src={images[i]} alt="" />
             <div className="pListTitle">
-                <h1>Hotels</h1>
-                <h2>2351 hotels</h2>
+                <h1>{data[i]?.type}</h1>
+                <h2>{data[i]?.count} {data[i]?.type}s</h2>
             </div>
         </div>
-        <div className="pListItem">
-            <img class="pListImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/119467716.jpeg?k=63b69100225782d08fbd4d0205bf949c0be894ab946a0366edb8ad48e9c0ef46&o=" alt="" />
-            <div className="pListTitle">
-                <h1>Apartments</h1>
-                <h2>1263 apartments</h2>
-            </div>
-        </div>
-        <div className="pListItem">
-            <img class="pListImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450084.jpeg?k=f8c2954e867a1dd4b479909c49528531dcfb676d8fbc0d60f51d7b51bb32d1d9&o=" alt="" />
-            <div className="pListTitle">
-                <h1>Resorts</h1>
-                <h2>856 hotels</h2>
-            </div>
-        </div>
-        <div className="pListItem">
-            <img class="pListImg"src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/100235855.jpeg?k=61ef6692e05b5971e2e8dc75687f844e6d0ad295a9a5ace17f7c713f167e61b5&o=" alt="" />
-            <div className="pListTitle">
-                <h1>Villas</h1>
-                <h2>1623 hotels</h2>
-            </div>
-        </div>
-        <div className="pListItem">
-            <img class="pListImg" src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450074.jpeg?k=7039b03a94f3b99262c4b3054b0edcbbb91e9dade85b6efc880d45288a06c126&o=" alt="" />
-            <div className="pListTitle">
-                <h1>Cottages</h1>
-                <h2>936 cottages</h2>
-            </div>
-        </div>
+        ))}
+        
+        </>)}
 
     </div>
   )
