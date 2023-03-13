8b90b5f98dedec87c185372806077eb63e5b8e13
peppemig <120139042+peppemig@users.noreply.github.com>
2023-03-14 00:53:58 +0100

    Update hotel.js

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -46,7 +46,7 @@ export const getHotel = async (req,res,next) => {
 export const getAllHotels = async (req,res,next) => {
     const { min, max, limit, ...others} = req.query
     try {
-        const hotels = await Hotel.find({ ...others, cheapestPrice: { $gte: min | 1, $lte: max || 999 }}).limit(req.query.limit);
+        const hotels = await Hotel.find({ ...others, cheapestPrice: { $gte: min || 1, $lte: max || 999 }}).limit(req.query.limit);
           res.status(200).json(hotels);
     } catch(err) {
         next(err)
